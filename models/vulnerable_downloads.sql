0228c6939834eed8e65b925b43b9d97eb4bde1bb
Paul Brabban <paul.brabban@gmail.com>
2024-01-03 12:11:47 +0000

    Create view of vulnerable downloads

---
+++ models/vulnerable_downloads.sql
@@ -0,0 +1,29 @@
+WITH grouped_vulnerabilities AS (
+    SELECT
+        download_date,
+        package,
+        package_version,
+        installer,
+        download_count,
+        ARRAY_AGG(STRUCT(cve, was_known_vulnerable_when_downloaded)) maybe_cves
+    FROM {{ ref('downloads_with_vulnerabilities') }}
+    GROUP BY
+        download_date,
+        package,
+        package_version,
+        installer,
+        download_count
+),
+
+filtered_by_vulnerable AS (
+    SELECT
+        *,
+        ARRAY(SELECT cve.cve FROM UNNEST(maybe_cves) cve WHERE cve.was_known_vulnerable_when_downloaded) vulnerabilities
+    FROM grouped_vulnerabilities
+)
+
+SELECT
+    * EXCEPT (maybe_cves),
+    ARRAY_LENGTH(vulnerabilities) cve_count,
+    ARRAY_LENGTH(vulnerabilities) > 0 was_vulnerable
+FROM filtered_by_vulnerable
