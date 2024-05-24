9b40b21d625d9b047c2abddf20f6a6c599045b10
Paul Brabban <paul.brabban@gmail.com>
2024-05-24 08:57:44 +0100

    Update test_safety_until_date for_withdrawn_cves_is_correct.sql

--- tests/test_safety_until_date for_withdrawn_cves_is_correct.sql
+++ tests/test_safety_until_date for_withdrawn_cves_is_correct.sql
@@ -5,6 +5,6 @@ WHERE cve = 'CVE-2022-33124'
     AND package = 'aiohttp'
     AND NOT (
         commit_date = '2023-04-01'
-        AND until_date = '2023-05-01'
+        AND until_date = '2024-05-01' --CVE un-withdrawn by the looks of it, but test still checks there's a gap
+    
     )
-    AND commit_date < '2024-02-03' -- ignore any subsequent changes
