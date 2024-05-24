10bf137ba0c5abc2ddc52d455149b9be70f8098f
Paul Brabban <paul.brabban@gmail.com>
2024-05-24 09:02:36 +0100

    Update test_safety_until_date for_withdrawn_cves_is_correct.sql

--- tests/test_safety_until_date for_withdrawn_cves_is_correct.sql
+++ tests/test_safety_until_date for_withdrawn_cves_is_correct.sql
@@ -6,5 +6,5 @@ WHERE cve = 'CVE-2022-33124'
     AND NOT (
         commit_date = '2023-04-01'
         AND until_date = '2024-05-01' --CVE un-withdrawn by the looks of it, but test still checks there's a gap
-    
     )
+    AND commit_date < '2024-05-01' -- ignore subsequent changes
