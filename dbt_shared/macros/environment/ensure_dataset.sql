11f39172b54c2a459aca72d1140950405a7a0ec6
Paul Brabban <paul.brabban@gmail.com>
2025-11-28 20:28:51 +0000

    add temp env var, grant public when prod

--- dbt_shared/macros/environment/ensure_dataset.sql
+++ dbt_shared/macros/environment/ensure_dataset.sql
@@ -1,4 +1,4 @@
-{% macro ensure_dataset(region='US') -%}
+{% macro ensure_dataset(region='US', is_public=False) -%}
 {% if execute %}
     {% set query %}
         {% set dataset_ref = '`' ~ target.database ~ '.' ~ target.schema ~ '`' %}
@@ -11,5 +11,16 @@
         )
     {% endset %}
     {% do run_query(query) %}
+
+    {%- set grant_option = 'GRANT' if is_public else 'REVOKE' -%}
+    {%- set grant_to_from = 'TO' if is_public else 'FROM' -%}
+    {%- set grant_sql -%}
+    {{ grant_option }} `roles/bigquery.dataViewer`
+    ON SCHEMA {{ dataset_ref }}
+    {{ grant_to_from }} 'specialGroup:allUsers'
+    {%- endset -%}
+
+    {%- do run_query(grant_sql) -%}
+    
 {% endif %}
 {%- endmacro %}
